5eced0c544434c2db3fa18290f58c479ec11c008
Rohit Anantha <anantha.rohit.11@gmail.com>
2024-04-08 16:17:23 -0500

    still an issue with the change password but it technically works, just doesn't reauthenticate properly yet

--- Swiftly/ViewControllers/AccountViewController.swift
+++ Swiftly/ViewControllers/AccountViewController.swift
@@ -18,19 +18,35 @@ class AccountViewController: UIViewController {
     
     var password = ""
     
-    @IBOutlet weak var newPasswordField: UITextField!
-    @IBOutlet weak var confirmNewPassword: UITextField!
-    
+    @IBOutlet weak var textLabel: UILabel!
     override func viewWillAppear(_ animated: Bool) {
         view.backgroundColor = UIColor.white
         self.tabBarController?.tabBar.isHidden = true
     }
     
     
-    @IBAction func saveChangesPressed(_ sender: Any) {
+    func reauthenticateAndUpdate(password:String, email:String, newpassword:String){
+        
+        let user = Auth.auth().currentUser
+        let credential: AuthCredential = EmailAuthProvider.credential(withEmail: (user?.email)!, password: password)
+        print(credential)
+        print(email, password, newpassword)
+        user?.reauthenticate(with: credential){ error, _  in
+            if let error = error {
+                print(" error : " + error.description)
+                user!.updatePassword(to: newpassword)
+            }
+            else {
+                print("successfully reauth")
+                user!.updatePassword(to: newpassword)
+            }
+        }
+    }
+    
+    @IBAction func changePasswordPressed(_ sender: Any) {
         let controller = UIAlertController(
-            title: "Alert",
-            message: "These features have not been implemented and only for display",
+            title: "Change Password",
+            message: "Please confirm your credentials",
             preferredStyle: UIAlertController.Style.alert)
         
         controller.addTextField{(textField) in
@@ -40,21 +56,32 @@ class AccountViewController: UIViewController {
         
         controller.addTextField{(textField) in
         
-            textField.placeholder = "Enter Password"
+            textField.placeholder = "Enter Current Password"
+            textField.isSecureTextEntry = true
+        }
+        
+        controller.addTextField{(textField) in
+        
+            textField.placeholder = "Enter New Password"
+            textField.isSecureTextEntry = true
+        }
+        
+        controller.addTextField{(textField) in
+        
+            textField.placeholder = "Reenter New Password"
+            textField.isSecureTextEntry = true
         }
         
         
         let login = UIAlertAction(title: "Re-Login", style: .default) { (alertAction) in
-            let username = controller.textFields![0] as UITextField
-            let password = controller.textFields![1] as UITextField
-            self.password = password.text!
-            
-            guard self.newPasswordField.text == self.confirmNewPassword.text else{
-                
-                print("Passwords Do Not Match!")
-                return
+            let email = controller.textFields![0].text
+            let password = controller.textFields![1].text
+            let newPassword = controller.textFields![2].text
+            let confirmNewPassword = controller.textFields![3].text
+            if newPassword != confirmNewPassword {
+                self.textLabel.text = "New Passwords do not Match!"
             }
-            self.reauthenticateAndUpdate(password: self.password)
+            self.reauthenticateAndUpdate(password: password!, email: email!, newpassword: newPassword!)
             
             
         }
@@ -62,79 +89,11 @@ class AccountViewController: UIViewController {
         
         present(controller, animated: false)
         
-        
-        
     }
-    func reauthenticateAndUpdate(password:String){
-        
-        let user = Auth.auth().currentUser
-        let credential: AuthCredential  = EmailAuthProvider.credential(withEmail: user?.email ?? "", password: password)
-
-        // Prompt the user to re-provide their sign-in credentials
-
-        user?.reauthenticate(with: credential) { error, _  in
-          if let error = error {
-            // An error happened.
-              print("Credential Error" + error.debugDescription)
-          } else {
-            // User re-authenticated.
-              if(self.newPasswordField.text != ""){
-                  Auth.auth().currentUser?.updatePassword(to: self.newPasswordField.text!)
-                  
-//                  RUNS WHEN SUCCEEDS?
-//                  { (error) in
-//                      print("Update Password Error: ")
-//                  }
-              }
-              
-//              if(Check username) and Update It!
-              
-              self.navigationController?.popViewController(animated: false)
-          }
-        }
+    @IBAction func changeUsernamePressed(_ sender: Any) {
     }
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-
-
-//        // Do any additional setup after loading the view.
-//        let controller = UIAlertController(
-//            title: "Alert",
-//            message: "These features have not been implemented and only for display",
-//            preferredStyle: .alert)
-//
-//        controller.addAction(UIAlertAction(title: "Go Back", style: .destructive){_ in
-//            self.navigationController?.popViewController(animated: false)
-//
-//            })
-//
-//
-//
-//        controller.addAction(UIAlertAction(title: "OK!", style: .default){_ in
-//
-//        })
-//
-//        present(controller,animated: true)
-        
     }
-    
-    @IBAction func cancelPressed(_ sender: Any) {
-        self.navigationController?.popViewController(animated: false)
-
-    }
-    
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
