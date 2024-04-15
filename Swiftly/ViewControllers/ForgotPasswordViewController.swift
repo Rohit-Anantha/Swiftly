65b52cdcb52b419dac15a9fd7ddc70dc806a956d
Rohit Anantha <anantha.rohit.11@gmail.com>
2024-04-15 14:35:36 -0500

    fixed some issues with the keyboard popups and the forgot password vc not being in the repo?

--- Swiftly/ViewControllers/ForgotPasswordViewController.swift
+++ Swiftly/ViewControllers/ForgotPasswordViewController.swift
@@ -8,14 +8,26 @@
 import UIKit
 
 import FirebaseAuth
-class ForgotPasswordViewController: UIViewController {
+class ForgotPasswordViewController: UIViewController, UITextFieldDelegate {
 
     @IBOutlet weak var emailBox: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
+        emailBox.delegate = self
 
         // Do any additional setup after loading the view.
     }
+    
+    func textFieldShouldReturn(_ textField:UITextField) -> Bool {
+            textField.resignFirstResponder()
+            return true
+        }
+        
+        // Called when the user clicks on the view outside of the UITextField
+
+        override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+            self.view.endEditing(true)
+        }
     
     @IBAction func forgotPasswordPressed(_ sender: Any) {
         Auth.auth().sendPasswordReset(withEmail: emailBox.text!){ error in
